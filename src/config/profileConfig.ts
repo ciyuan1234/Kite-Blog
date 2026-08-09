8d43d1e80999c72cce647f6f255da67bffab65df
秋安在coding <3085197557@qq.com>
2026-08-09 23:13:27 +0800

    config: update profile settings

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -1,14 +1,14 @@
 import type { ProfileConfig } from "../types/profileConfig";
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.avif",
+	avatar: "https://pic2.zhimg.com/v2-c21606cc260d1e83272a3b3243a2ff17_r.jpg",
 	name: "Kite",
 	bio: "写技术、项目、生活和一些长期问题。",
 	links: [
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/ciyuan1234",
+			url: "https://pic2.zhimg.com/v2-c21606cc260d1e83272a3b3243a2ff17_r.jpg",
 			showName: false,
 		},
 		{
@@ -16,6 +16,6 @@ export const profileConfig: ProfileConfig = {
 			icon: "fa7-solid:rss",
 			url: "/rss/",
 			showName: false,
-		},
+		}
 	],
 };
